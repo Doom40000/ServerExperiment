79d5b18b4b9d3671ac3122647c2619493e3b7ef1
Doom40000 <richardbaker1993@gmail.com>
2024-03-17 20:06:32 +0000

    added app.js and package.json

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.write("On our way to being a full stack engineer");
+  res.end();
+}).listen(3000);
+
+console.log("Server started on 3000!");
+
